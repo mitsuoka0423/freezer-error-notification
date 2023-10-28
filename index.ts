69e65644fc4472a6339477793838a77393ea0842
光岡 高宏/MITSUOKA Takahiro <61106530+mitsuoka0423@users.noreply.github.com>
2023-10-28 21:00:59 +0000

    feat: upd message

--- index.ts
+++ index.ts
@@ -10,19 +10,114 @@ const switchbot = new SwitchBot({
   secretKey: Bun.env.SWITCH_BOT_CLIENT_SECRET || "",
 });
 
+console.log('冷凍庫の温度を取得します');
+
 const meter = await switchbot.meter("E05C356C390D");
 const temperatue = await meter.getTemperature();
 const humidity = await meter.getHumidity();
 
-console.log({
-  temperatue, humidity
-});
+console.log({ temperatue, humidity });
 
-if (temperatue > -5) {
+if (temperatue < -5) {
+  console.log("問題なし");
+} else {
   await lineClient.broadcast({
     messages: [{
-      type: "text",
-      text: `冷凍庫の温度が上がっています。確認してください。\n温度: ${temperatue}`
+      type: "flex",
+      altText: "冷凍庫温度上昇アラート",
+      contents: {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://i.ibb.co/WVY4RKg/alert.png",
+          "aspectMode": "fit",
+          "offsetTop": "10px"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "温度上昇アラート！",
+              "weight": "bold",
+              "size": "xl"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "margin": "lg",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "spacing": "sm",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "冷凍庫の温度が上がっています",
+                      "wrap": true,
+                      "color": "#666666",
+                      "size": "sm",
+                      "flex": 5
+                    }
+                  ]
+                },
+                {
+                  "type": "separator",
+                  "margin": "10px"
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "spacing": "sm",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "現在の温度",
+                      "color": "#aaaaaa",
+                      "size": "sm",
+                      "flex": 2
+                    },
+                    {
+                      "type": "text",
+                      "text": `${temperatue}℃`,
+                      "wrap": true,
+                      "color": "#666666",
+                      "size": "sm",
+                      "flex": 4
+                    }
+                  ],
+                  "margin": "10px"
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "正常温度",
+                      "color": "#aaaaaa",
+                      "size": "sm",
+                      "flex": 2
+                    },
+                    {
+                      "type": "text",
+                      "text": "-15〜-20℃",
+                      "wrap": true,
+                      "color": "#666666",
+                      "size": "sm",
+                      "flex": 4
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
     }]
   });
+  console.log("LINEにメッセージを送信しました");
 }
