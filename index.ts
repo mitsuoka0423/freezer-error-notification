7e7f6e4acf480c8920abf0447b7f85c92e499950
光岡 高宏/MITSUOKA Takahiro <61106530+mitsuoka0423@users.noreply.github.com>
2023-10-28 20:37:29 +0000

    feat: add line broadcast

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import * as line from '@line/bot-sdk';
+import SwitchBot from "@lucadiba/switchbot-client";
+
+const lineClient = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: Bun.env.LINE_CHANNEL_ACCESS_TOKEN || "",
+});
+
+const switchbot = new SwitchBot({
+  openToken: Bun.env.SWITCH_BOT_TOKEN || "",
+  secretKey: Bun.env.SWITCH_BOT_CLIENT_SECRET || "",
+});
+
+const meter = await switchbot.meter("E05C356C390D");
+const temperatue = await meter.getTemperature();
+const humidity = await meter.getHumidity();
+
+console.log({
+  temperatue, humidity
+});
+
+if (temperatue > -5) {
+  await lineClient.broadcast({
+    messages: [{
+      type: "text",
+      text: `冷凍庫の温度が上がっています。確認してください。\n温度: ${temperatue}`
+    }]
+  });
+}
